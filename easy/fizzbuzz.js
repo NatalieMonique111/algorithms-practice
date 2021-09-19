09c7e6429301f14b499e3ae6333fe0fb4d7d4ae9
NatalieMonique111 <nataliemonique111@gmail.com>
2021-09-18 20:14:48 -0700

    fizzbuzz

---
+++ easy/fizzbuzz.js
@@ -0,0 +1,17 @@
+// create a function that counts from 1 to num
+// replace each number that is divisible by a certain number of strings
+
+
+function fizzbuzz(num) {
+  for (let i = 0; i < num; i++) {
+    if ( i % 15 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+    console.log("fizz");
+    } else if (i % 5 === 0){
+    console.log("buzz");
+    } else {
+    console.log(i);
+  }
+}
+};
