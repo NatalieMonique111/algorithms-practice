276408a81350e04bb136efb232b5a52a4f9462a1
NatalieMonique111 <nataliemonique111@gmail.com>
2021-11-12 19:16:59 -0800

    added to challenge

--- study-group/sortArray.js
+++ study-group/sortArray.js
@@ -6,3 +6,12 @@ const sortArray = (array) => {
   });
 };
 console.log("result", sortArray([4.5, 70, -9, 100]));
+
+//Sort an Array of Numbers in ascending order
+
+const sortArray = (array) => {
+  return array.sort(function (a, b) {
+    return a - b;
+  });
+};
+console.log("result", sortArray([4.5, 70, -9, 100]));
