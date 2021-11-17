b0287ffd741bc9319066e9c31d3f2313228ba8b4
NatalieMonique111 <nataliemonique111@gmail.com>
2021-11-17 14:23:27 -0800

    added imperative example

--- easy/arrayIntersection.js
+++ easy/arrayIntersection.js
@@ -1,19 +1,18 @@
-let arr1 = ["A", "B", "Z"];
-let arr2 = ["Z", "X", "Y"];
+/////////////////DECLARATIVE -Finds common elements, DOES NOT REMOVE DUPLICATES-///////////////
+//Notes:
+// -The includes() method determines whether an array includes a certain value among its entries, returning true or false.
+// -filter() method creates a new array with all elements that pass the test implemented by the provided function.
+// -char is every element in the array
+//-is a linear function. O(n)
 
-// The includes() method determines whether an array includes a certain value among its entries, returning true or false as appropriate.
+let arr1 = ["A", "B", "Z", "Z"];
+let arr2 = ["Z", "X", "Y", "Z"];
 
-// filter() method creates a new array with all elements that pass the test implemented by the provided function.
-
-// n is every element in the array
 const intersection = arr1.filter((char) => arr2.includes(char));
-//filter checks fn, return true value
-
 console.log(intersection);
 
-//is a linear function. O(n)
+//////////////////////DECLARATIVE -Finds common element, REMOVES DUPLICATES- ////////////////////
 
-/////////////////////////////////////////////////
 //https://leetcode.com/problems/intersection-of-two-arrays/submissions/
 
 let nums1 = ["A", "B", "Z", "Z"];
@@ -27,3 +26,18 @@ var intersection = function (nums1, nums2) {
 };
 
 console.log(intersection(nums1, nums2));
+
+////////////////////IMPERATIVE with for loop//////////////////
+//Does NOT remove duplicates
+function sameElements(arr1, arr2) {
+  let result = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2.includes(arr1[i])) {
+      result.push(arr1[i]);
+    }
+  }
+  return result;
+}
+
+console.log(sameElements([1, 2, 3, 4], [3, 4, 5]));
