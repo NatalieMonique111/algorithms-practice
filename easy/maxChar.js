f474950cc3bfd5d0d76d12b32dc64e5444024b24
NatalieMonique111 <nataliemonique111@gmail.com>
2021-11-18 21:13:32 -0800

    edited function name and re-named variables

--- easy/maxChar.js
+++ easy/maxChar.js
@@ -1,38 +1,41 @@
 // Most used letter in a string
 // https://stackoverflow.com/questions/22590023/finding-the-most-frequent-character-in-a-string-javascript
 
-  /**
-     * i.e. doggs
-     * charMap = {
-     * d: 1,
-     * o: 1,
-     * g: 2,
-     * }
-     *  
-     * */ 
-   function maxChar(str) {
+//  i.e. doggs
+//   charMap = {
+//   d: 1,
+//   o: 1,
+//   g: 2,
+//   }
 
-    const charMap = {};
-    let max = 0;
-    let maxChar = '';
+function getMaxChar(str) {
+  let charMap = {};
+  let charCount = 0;
+  let maxChar = "";
 
-    for(let char of str){ // [d,o,g,g]
-        if(charMap[char]){ // {d:1} is "d" in the map already?
-            charMap[char]++; // increment the value,  
-        }else{ // d is not in map, add it: {d: 1}
-            // every char will be assigned value of 1 at first appearance
-            charMap[char] = 1;
-        }
+  //iterating through the string and populating the hash map
+  for (let char of str) {
+    // [d,o,g,g]
+    if (charMap[char]) {
+      // {d:1} is "d" in the map already?
+      charMap[char]++; // increment the value,
+    } else {
+      // d is not in map, add it: {d: 1}
+      // every char will be assigned value of 1 at first appearance
+      charMap[char] = 1;
     }
-
-    for(let char in charMap){ // 'd', 'o', 'g'
-        if(charMap[char] > max){ // compare each number, ie. 1 or 2
-            max = charMap[char];
-            maxChar = char;
-        }
+  }
+  //this loop iterates through the object and sets char count to the most frequent char
+  for (let char in charMap) {
+    // 'd', 'o', 'g'
+    if (charMap[char] > charCount) {
+      // compare each number/value, ie. 1 or 2
+      charCount = charMap[char];
+      //console.log(charMap[char], char);
+      maxChar = char;
     }
-
-    return maxChar; 
+  }
+  return maxChar;
 }
 
-maxChar('dogg');
+console.log(getMaxChar("dogg"));
