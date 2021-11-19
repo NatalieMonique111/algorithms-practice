e63ffd833146c143d9dca469a585ddb667102b0c
NatalieMonique111 <nataliemonique111@gmail.com>
2021-11-18 18:04:36 -0800

    added regex solution

--- technical-interview/reverseVowels.js
+++ technical-interview/reverseVowels.js
@@ -56,3 +56,61 @@ console.log(reverseVowel("pug dog"));
 console.log(reverseVowel(undefined));
 console.log(reverseVowel("a"));
 console.log(reverseVowel(123));
+
+
+
+//////////////////////Optimal Solution///////////////////////
+https://www.geeksforgeeks.org/reverse-vowels-given-string/
+
+// JavaScript program to reverse order of vowels
+     
+    // utility function to check for vowel
+    function isVowel(c) {
+      return (c == 'a' || c == 'A' || c == 'e'
+              || c == 'E' || c == 'i' || c == 'I'
+              || c == 'o' || c == 'O' || c == 'u'
+              || c == 'U');
+  }
+ 
+  // Function to reverse order of vowels
+  function reverseVowel(str1) {
+      let j = 0;
+      // Storing the vowels separately
+      let str = str1.split('');
+      let vowel = "";
+      for (let i = 0; i < str.length; i++) {
+          if (isVowel(str[i])) {
+              j++;
+              vowel += str[i];
+          }
+      }
+ 
+      // Placing the vowels in the reverse
+      // order in the string
+      for (let i = 0; i < str.length; i++) {
+          if (isVowel(str[i])) {
+              str[i] = vowel[--j];
+          }
+      }
+ 
+      return str.join("");
+  }
+   
+  let str = "hello world";
+    document.write(reverseVowel(str));
+  
+
+    //////////////////////////////Using REGEX/////////////////////////
+
+    function reverseVowels(str){
+      var vowels = str.match(/[aeiou]/g);
+      return str.replace(/[aeiou]/g, () => vowels.pop());
+   }
+   
+   // example
+   var original = 'James Bond';
+   var result = reverseVowels(original);
+   
+   // output for snippet
+   console.log( `${original}->`,result);
+   
