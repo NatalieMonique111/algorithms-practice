887166b555f1104a7e25dfd45fcf3b3580ad62a7
NatalieMonique111 <nataliemonique111@gmail.com>
2021-11-21 19:30:39 -0800

    edited file

--- edabit-code-challenge/arrayJackpot.js
+++ edabit-code-challenge/arrayJackpot.js
@@ -1,17 +1,17 @@
-// Hitting the Jackpot
+// Hitting the Jackpot!
 // Create a function that takes in an array (slot machine outcome) and returns true if all elements in the array are identical, and false otherwise. The array will contain 4 elements.
-
-// Examples
-// testJackpot(["@", "@", "@", "@"]) ➞ true
-
-// testJackpot(["abc", "abc", "abc", "abc"]) ➞ true
-
-// testJackpot(["SS", "SS", "SS", "SS"]) ➞ true
-
-// testJackpot(["&&", "&", "&&&", "&&&&"]) ➞ false
-
-// testJackpot(["SS", "SS", "SS", "Ss"]) ➞ false
 
 function testJackpot(result) {
   return result.every((x) => x === result[0]);
 }
+
+// Examples
+testJackpot(["@", "@", "@", "@"]); // ➞ true
+
+testJackpot(["abc", "abc", "abc", "abc"]); // ➞ true
+
+testJackpot(["SS", "SS", "SS", "SS"]); // ➞ true
+
+testJackpot(["&&", "&", "&&&", "&&&&"]); // ➞ false
+
+testJackpot(["SS", "SS", "SS", "Ss"]); // ➞ false
