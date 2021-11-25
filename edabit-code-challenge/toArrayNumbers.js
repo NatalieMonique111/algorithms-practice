22e231c69bff52cc5f88e583f25e867bc5474ec3
NatalieMonique111 <nataliemonique111@gmail.com>
2021-11-24 21:06:14 -0800

    added notes

--- edabit-code-challenge/toArrayNumbers.js
+++ edabit-code-challenge/toArrayNumbers.js
@@ -1,18 +1,7 @@
 //Array of Strings to Array of Numbers
-
 // Create a function that takes as a parameter an array of "stringified" numbers and returns an array of numbers.
 
-// Example:
-
-// ["1", "3", "3.6"] ➞ [1, 3, 3.6]
-// Examples
-// toNumberArray(["9.4", "4.2"]) ➞ [9.4, 4.2]
-
-// toNumberArray(["91", "44"]) ➞ [91, 44]
-
-// toNumberArray(["9.5", "8.8"]) ➞ [9.5, 8.8]
-// Notes
-// Some inputs are floats.
+// Example: ["1", "3", "3.6"] ➞ [1, 3, 3.6]
 
 function toNumberArray(arr) {
   let newArr = [];
@@ -21,3 +10,16 @@ function toNumberArray(arr) {
   }
   return newArr;
 }
+
+// Examples
+
+// toNumberArray(["9.4", "4.2"]) ➞ [9.4, 4.2]
+
+// toNumberArray(["91", "44"]) ➞ [91, 44]
+
+// toNumberArray(["9.5", "8.8"]) ➞ [9.5, 8.8]
+
+// Notes
+// Some inputs are floats.
+//why use parseFloat() instead of parseInt(): Aninteger has no decimal part, so any number "casted" to int will lose its //decimals. You should use parseFloat if you want to keep the decimal part.
+//https://stackoverflow.com/questions/28894971/problems-with-javascript-parseint-decimal-string
