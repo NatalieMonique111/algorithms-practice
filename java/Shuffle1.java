0d557b4437144a99f0f7ddd3b52aca50a92f9d9e
NatalieMonique111 <nataliemonique111@gmail.com>
2021-12-02 10:39:24 -0800

    added shuffle1

---
+++ java/Shuffle1.java
@@ -0,0 +1,21 @@
+//https://www.rcsdk12.org/cms/lib/NY01001156/Centricity/Domain/4951/Head_First_Java_Second_Edition.pdf
+public class Shuffle1 {
+ public static void main(String[] args){
+   int x = 3;
+   while (x > 0) {
+    if (x > 2){
+      System.out.print("a");
+    }
+    if ( x == 2 ) {
+      System.out.print("b c");
+    }
+    if (x == 1) {
+      System.out.print("d");
+      x = x - 1;
+      }
+      x = x - 1;
+      System.out.print("-");
+   }
+ }
+
+}
